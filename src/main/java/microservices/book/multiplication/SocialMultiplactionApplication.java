a127f8083b0bcb8f1654960c1f27889a1c42b251
Steven Wohl <steverwohl@gmail.com>
2018-08-28 14:50:33 +0200

    intitial commit

---
+++ src/main/java/microservices/book/multiplication/SocialMultiplactionApplication.java
@@ -0,0 +1,12 @@
+package microservices.book.multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMultiplactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMultiplactionApplication.class, args);
+	}
+}
